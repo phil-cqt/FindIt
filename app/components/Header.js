0cd40b8a8a217d253e93c4b8e27cd9c8e42b499a
phil-cqt <philemon.coquet@edu.ece.fr>
2024-04-09 20:59:43 +0200

    fix: fixed deployment

--- app/components/Header.js
+++ app/components/Header.js
@@ -18,7 +18,7 @@ const Header = ({ onClickLogin }) => {
         
         {/* About button */}
         <Link href="/about" passHref>
-          <button className="text-sm font-bold hover:text-gray-300 focus:outline-none font-bold">&Agrave; propos</button>
+          <button className="text-sm font-bold hover:text-gray-300 focus:outline-none font-bold">A propos</button>
         </Link>
       </div>
     </div>
